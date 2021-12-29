990cc8a35425848012203e18f1d9b21a8a319680
Paulo Alenquer <pauloalenquer@gmail.com>
2021-12-29 16:31:22 -0300

    global next configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Header from "../components/Header";
+import "./styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container md:mx-auto md:px-40 px-8 py-8 text-lighter font-roboto min-h-screen flex flex-col">
+      <Header />
+
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
